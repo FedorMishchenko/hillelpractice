8bd8788452b6919822688545edb367974c0a0d69
FedorMishchenko <fedormishchenko.ua@gmail.com>
2018-11-18 19:58:41 +0200

    My second commit

--- src/com/hillel/fmishchenko_practice/Main.java
+++ src/com/hillel/fmishchenko_practice/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("My first commit");
+        System.out.println("My second commit");
     }
 }
