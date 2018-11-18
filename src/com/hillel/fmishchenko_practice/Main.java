9f214917377990077177fb4f70c2e2b39dfbb50a
FedorMishchenko <fedormishchenko.ua@gmail.com>
2018-11-18 19:52:48 +0200

    My first commit.

---
+++ src/com/hillel/fmishchenko_practice/Main.java
@@ -0,0 +1,10 @@
+package com.hillel.fmishchenko_practice;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My first commit");
+    }
+}
