8a9e88ba0ddfd17c85f864fc1d579b7326131dd3
fmishchenko <fedormishchenko.ua@gmail.com>
2018-12-18 22:15:13 +0200

    commit

---
+++ src/com/hillel/fmishchenko_practice/homeWork_6_Task_1/List.java
@@ -0,0 +1,39 @@
+package com.hillel.fmishchenko_practice.homeWork_6_Task_1;
+
+
+import java.util.Iterator;
+
+public class List<T> implements Iterable<T>{
+
+    private T[] arrayList;
+    private int currentSize;
+
+    public List(T[] newArray) {
+        this.arrayList = newArray;
+        this.currentSize = arrayList.length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> it = new Iterator<T>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < currentSize && arrayList[currentIndex] != null;
+            }
+
+            @Override
+            public T next() {
+                return arrayList[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+}
---
+++ src/com/hillel/fmishchenko_practice/homeWork_6_Task_1/ListIterator.java
@@ -0,0 +1,15 @@
+package com.hillel.fmishchenko_practice.homeWork_6_Task_1;
+
+import java.util.Iterator;
+
+public class ListIterator<T> implements Iterator<T> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
