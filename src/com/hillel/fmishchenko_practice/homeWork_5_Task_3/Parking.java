54c0f8e53d6fafa9a7d40ccfc384731b031380df
fmishchenko <fedormishchenko.ua@gmail.com>
2018-12-18 21:39:38 +0200

    commit

--- src/com/hillel/fmishchenko_practice/homeWork_5_Task_3/Parking.java
+++ src/com/hillel/fmishchenko_practice/homeWork_5_Task_3/Parking.java
@@ -1,18 +1,12 @@
 package com.hillel.fmishchenko_practice.homeWork_5_Task_3;
 
+
 import java.util.HashMap;
 
-public class Parking<key, Auto> extends HashMap<key,Auto> {
-
-
+public class Parking<key,Car> extends HashMap<key,Car> {
 
     public Parking() {
-        super();
+        
     }
-
-    public void sort() {
-    }
-
-
 
 }
--- src/com/hillel/fmishchenko_practice/homeWork_5_Task_3/ParkingDemo.java
+++ src/com/hillel/fmishchenko_practice/homeWork_5_Task_3/ParkingDemo.java
@@ -1,20 +1,28 @@
 package com.hillel.fmishchenko_practice.homeWork_5_Task_3;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
 public class ParkingDemo {
 
     public static void main(String[] args) {
         Parking parking = new Parking();
-        Auto reno = new Auto("reno");
-        Auto citroen = new Auto("citroen");
-        Auto peugeot = new Auto("peugeot");
+        Car reno = new Car("reno");
+        Car citroen = new Car("citroen");
+        Car peugeot = new Car("peugeot");
         parking.put(reno.getKey(),reno);
+        System.out.println("Arrival " + reno.getModel());
         parking.put(citroen.getKey(),citroen);
+        System.out.println("Arrival " + citroen.getModel());
         parking.put(peugeot.getKey(),peugeot);
+        System.out.println("Arrival " + peugeot.getModel());
+        System.out.println("Cars on parking: " + parking.size());
+        parking.remove(peugeot.getKey());
+        System.out.println(peugeot.getModel() + " has departure");
+        System.out.println("Is " + peugeot.getModel() +
+                " on parking: "+ parking.containsValue(peugeot));
+        System.out.println("Cars on parking: " + parking.size());
+        parking.clear();
+        System.out.println(parking.size());
+
 
 
 
