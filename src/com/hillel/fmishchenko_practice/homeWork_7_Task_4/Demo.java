9979a61423f957202079034fa074cb3ec4ae3240
fmishchenko <fedormishchenko.ua@gmail.com>
2018-12-25 19:51:28 +0200

    commit

--- src/com/hillel/fmishchenko_practice/homeWork_7_Task_4/Demo.java
+++ src/com/hillel/fmishchenko_practice/homeWork_7_Task_4/Demo.java
@@ -2,6 +2,9 @@ package com.hillel.fmishchenko_practice.homeWork_7_Task_4;
 
 public class Demo {
     public static void main(String[] args) {
-        
+        System.out.println(new HashDigest().hash("password", "MD5"));
+        System.out.println(new HashDigest().hash("password1", "SHA-256"));
+        System.out.println(new HashDigest().hash("password2", "SHA-512"));
+
     }
 }
