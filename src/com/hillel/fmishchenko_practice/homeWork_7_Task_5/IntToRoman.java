bf260ef13146053e4b100ceaad6a8256841790ba
fmishchenko <fedormishchenko.ua@gmail.com>
2018-12-25 20:05:38 +0200

    commit

--- src/com/hillel/fmishchenko_practice/homeWork_7_Task_5/IntToRoman.java
+++ src/com/hillel/fmishchenko_practice/homeWork_7_Task_5/IntToRoman.java
@@ -3,6 +3,7 @@ package com.hillel.fmishchenko_practice.homeWork_7_Task_5;
 public class IntToRoman {
 
         public  String convert(int n) {
+            int tmp = n;
             String[] a = new String[]{"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
             int[] b = new int[]{1,4,5,9,10,40,50,90,100,400,500,900,1000};
             StringBuilder s = new StringBuilder();
@@ -12,7 +13,7 @@ public class IntToRoman {
                     n -= b[i];
                 }
             }
-            return s.toString();
+            return tmp + " ===> " + s.toString();
         }
 
 }
