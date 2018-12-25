56952734afbbd7d302f99bf99c92aa73536c7677
fmishchenko <fedormishchenko.ua@gmail.com>
2018-12-25 21:31:27 +0200

    commit

--- src/com/hillel/fmishchenko_practice/homeWork_8_Task_1/Demo.java
+++ src/com/hillel/fmishchenko_practice/homeWork_8_Task_1/Demo.java
@@ -2,6 +2,16 @@ package com.hillel.fmishchenko_practice.homeWork_8_Task_1;
 
 public class Demo {
     public static void main(String[] args) {
-
+        Dictionary dictionary = new Dictionary();
+        dictionary.put("Привет","Hello");
+        System.out.println(dictionary.toString("Привет"));
+        dictionary.put("Пока","Bye");
+        System.out.println(dictionary.toString("Пока"));
+        System.out.println(dictionary.getEn("Пока"));
+        System.out.println(dictionary.getRu("Hello"));
+        System.out.println("Dictionary size: " + dictionary.size());
+        dictionary.remove("Привет");
+        System.out.println(dictionary.toString("Привет"));
+        System.out.println("Dictionary size: " + dictionary.size());
     }
 }
---
+++ src/com/hillel/fmishchenko_practice/homeWork_8_Task_1/Dictionary.java
@@ -0,0 +1,53 @@
+package com.hillel.fmishchenko_practice.homeWork_8_Task_1;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Dictionary {
+    public Dictionary(){
+
+    }
+    private HashMap<String,String> dictionary = new HashMap<>();
+
+    public void put(String key,String value){
+        dictionary.put(key,value);
+    }
+    public String getEn(String key){
+        return "EN:  " + dictionary.get(key);
+    }
+    public String getRu(String key){
+        Set<Map.Entry<String,String>> entrySet = dictionary.entrySet();
+
+        String getValue = key;
+        for (Map.Entry<String,String> pair : entrySet) {
+            if (getValue.equals(pair.getValue())) {
+                return "RU:  " + pair.getKey();
+            }
+        }
+        System.out.println("Key is not exists");
+        return "";
+    }
+    public void remove(String key){
+        dictionary.remove(key);
+    }
+    public Set<String> keySet(){
+        return dictionary.keySet();
+    }
+    public Collection<String> values(){
+        return dictionary.values();
+    }
+    public int size(){
+        return dictionary.size();
+    }
+    public String toString(String key){
+        String ru = key;
+        String en = dictionary.get(key);
+        if(ru == null || en == null){
+            System.out.print("No such element");
+            return "";
+        }
+        return "RU:  " + ru + " ==> " + " EN: " + en;
+    }
+}
