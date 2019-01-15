21b2c4333cbd8474bd7441b63dd90f4575244e7b
fmishchenko <fedormishchenko.ua@gmail.com>
2019-01-15 21:04:29 +0200

    commit

--- src/com/hillel/fmishchenkopractice/homework8task1/Demo.java
+++ src/com/hillel/fmishchenkopractice/homework8task1/Demo.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 public class Demo {
     public static void main(String[] args) throws IOException {
         System.setProperty("console.encoding", "UTF-8");
-        ReaderManager manager = new ReaderManager(new CharReader());
-        manager.read();
+        new ReaderManager(new CharReader()).read();
     }
 }
