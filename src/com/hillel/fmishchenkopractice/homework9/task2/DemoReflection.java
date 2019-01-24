7c933bbd87de814c2c97ff678c8bc3e8fd173f92
fmishchenko <fedormishchenko.ua@gmail.com>
2019-01-24 13:30:34 +0200

    commit

--- src/com/hillel/fmishchenkopractice/homework9/task2/DemoReflection.java
+++ src/com/hillel/fmishchenkopractice/homework9/task2/DemoReflection.java
@@ -7,7 +7,7 @@ public class DemoReflection {
     private static Person person;
 
 
-    public static String getClassNameFromXML() {
+    private static String getClassNameFromXML() {
 
         String s = ClassLoader.getSystemClassLoader().getResource("resources.properties").getFile();
         File f = new File(s);
@@ -34,26 +34,23 @@ public class DemoReflection {
             try {
                 name = person.getClass().getDeclaredField("name");
                 name.setAccessible(true);
+                name.set(person, "Semen Semenich");
 
                 email = person.getClass().getDeclaredField("email");
                 email.setAccessible(true);
+                email.set(person, "gorbunkov@com");
 
                 address = person.getClass().getDeclaredField("address");
                 address.setAccessible(true);
+                address.set(person, "Dubrovka");
 
                 profession = person.getClass().getDeclaredField("profession");
                 profession.setAccessible(true);
+                profession.set(person, "contrabandist");
 
                 age = person.getClass().getDeclaredField("age");
                 age.setAccessible(true);
-
-                name.set(person, "Semen Semenich");
-                email.set(person, "gorbunkov@com");
-                address.set(person, "Dubrovka");
-                profession.set(person, "contrabandist");
                 age.set(person, Integer.parseInt("50"));
-
-
             } catch (NoSuchFieldException e) {
                 e.printStackTrace();
             }
