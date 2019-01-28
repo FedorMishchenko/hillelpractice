52ef34e1f11300e733199ee96eb296fc4b5b0283
fmishchenko <fedormishchenko.ua@gmail.com>
2019-01-28 11:56:39 +0200

    commit

--- src/exersices/java8/lambda/Lambda.java
+++ src/exersices/java8/lambda/Lambda.java
@@ -1,27 +1,24 @@
 package exersices.java8.lambda;
 
-
-import java.lang.reflect.Array;
-
 public class Lambda {
-
-    public Lambda(FuncInterface funcInterface) {
-    }
-
-    public Lambda(FuncInterface2 funcInterface2) {
-    }
 
     public static void main(String[] args) {
         FuncInterface<String, Integer> interface1 = (from) -> Integer.valueOf(from);
         Integer convertedNumber = interface1.abstractFunction("123");
         System.out.println(convertedNumber);
 
+        FuncInterface<String, Integer> interface3 = Integer::valueOf;
+        Integer convertedNumber2 = interface3.abstractFunction("456");
+        System.out.println(convertedNumber2);
+
         FuncInterface2<String> interface2 = (massege) -> {
-            massege += massege;
-            return massege;
+            int i = interface3.abstractFunction("123");
+            int j = interface1.abstractFunction("456");
+            return massege + " " + i + " " + j;
         };
         String str = interface2.abstractFunc2("Massege");
         System.out.println(str);
+        interface1.print("FuncInterface1");
 
 
     }
