222ecff18557294b9b6bc6ff5a0d49b5f8e84f6a
fmishchenko <fedormishchenko.ua@gmail.com>
2019-01-29 12:20:17 +0200

    commit

---
+++ src/com/hillel/fmishchenkopractice/homework5/task1/Demo.java
@@ -0,0 +1,22 @@
+package com.hillel.fmishchenkopractice.homework5.task1;
+
+import com.hillel.fmishchenkopractice.homework4.task1.part3.LinkedList;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Demo {
+    final static int MIN = 0;
+    final static int MAX = 100;
+        public static void main(String[] args) {
+            LinkedList<Integer> list = new LinkedList<>();
+            for (int i = 0; i < 10; i++){
+                list.add(ThreadLocalRandom.current().nextInt(MIN,MAX));
+            }
+            System.out.println("size = " + list.getSize());
+            for (int i = 0; i < list.getSize(); i++){
+                System.out.print(list.get(i) + ", ");
+            }
+
+
+    }
+}
