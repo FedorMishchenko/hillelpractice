fda100d4a63647ec5391c57710ab5d85edff0afe
fmishchenko <fedormishchenko.ua@gmail.com>
2019-02-02 23:43:57 +0200

    commit

--- src/com/hillel/fmishchenkopractice/homework10/task2/Demo.java
+++ src/com/hillel/fmishchenkopractice/homework10/task2/Demo.java
@@ -1,15 +1,14 @@
 package com.hillel.fmishchenkopractice.homework10.task2;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Demo {
     public static void main(String[] args) {
-        int size = 100_000;
+        final int size = 1_000_000;
         long start,end;
         int[] unsorted = new int[size];
-        Random random = new Random();
         for (int i = 0; i < size; i++){
-            unsorted[i] = random.nextInt(10_000);
+            unsorted[i] = ThreadLocalRandom.current().nextInt(0,10_000);
         }
         start = System.currentTimeMillis();
         new Merger(unsorted).sort();
@@ -17,15 +16,12 @@ public class Demo {
         print("Merger time: ", end - start + '\n');
         start = System.currentTimeMillis();
         for (int i = 0; i < size; i++){
-            unsorted[i] = random.nextInt(10_000);
+            unsorted[i] = ThreadLocalRandom.current().nextInt(0,10_000);
         }
         MultiMerger merger2 = new MultiMerger(unsorted);
         merger2.start();
         end = System.currentTimeMillis();
         print("MultiMerger time: ", end - start);
-    }
-
-    private static void print(String sorted) {
     }
 
     public static void print(String s, long l) {
