b6e9277134a1121f440b594cc3757a7508a37f50
fmishchenko <fedormishchenko.ua@gmail.com>
2019-02-27 17:25:56 +0200

    commit

--- src/com/hillel/fmishchenkopractice/homework12/restaurant/QueryManager.java
+++ src/com/hillel/fmishchenkopractice/homework12/restaurant/QueryManager.java
@@ -6,23 +6,42 @@ import java.util.logging.Logger;
 
 public class QueryManager {
     private static final Logger logger = Logger.getLogger(QueryManager.class.getName());
-    public String adminCreateQuery(BufferedReader reader)throws IOException{
-            System.out.println("Enter item:");
-            String item = reader.readLine();
-            System.out.println("Enter price:");
-            String price = reader.readLine();
-            return "INSERT INTO restaurant.menu (item, price)" +
-                    "VALUES ('" + item + "','" + price + "')";
-        }
-        public String adminReadQuery(){
-            return "SELECT id, item, price FROM restaurant.menu";
-        }
-        public String adminUpdateQuery(String id){
-        return "SELECT id, item, price FROM " +
-                    "restaurant.menu WHERE id = " + id;
-        }
-        public String adminDeleteQuery(String id){
-        return "DELETE FROM restaurant.menu WHERE id = " + id;
-        }
+
+    String adminCreateQuery(BufferedReader reader) throws IOException {
+        System.out.println("Enter item:");
+        String item = reader.readLine();
+        System.out.println("Enter price:");
+        String price = reader.readLine();
+        return "INSERT INTO restaurant.menu (item, price)" +
+                "VALUES ('" + item + "','" + price + "')";
     }
+
+    String adminReadQuery() {
+        return "SELECT id, item, price FROM restaurant.menu";
+    }
+
+    String adminUpdateQuery(String id) {
+        return "SELECT id, item, price FROM " +
+                "restaurant.menu WHERE id = " + id;
+    }
+
+    String adminDeleteQuery(String id) {
+        return "DELETE FROM restaurant.menu WHERE id = " + id;
+    }
+
+    String createOrderQuery() {
+        return null;
+    }
+
+    String readOrdersQuery(String id) {
+        return "SELECT ";
+    }
+    String updateOrderQuery(){
+        return null;
+    }
+    String deleteOrderQuery(){
+        return null;
+    }
+
+}
 
